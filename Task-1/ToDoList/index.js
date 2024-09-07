eeab48b3884715c7cfaebccb9c541588d7ce0a42
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-09-07 11:28:53 +0530

    changes done

---
+++ Task-1/ToDoList/index.js
@@ -0,0 +1,79 @@
+let tasks = [];
+
+const saveTasks = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+const addTask = () => {
+    const taskInput = document.getElementById('taskInput');
+    const text = taskInput.value.trim();
+
+    if(text){
+        tasks.push({text: text, completed: false});
+        //console.log(tasks);
+        taskInput.value = '';
+        updateTasksList();
+        updateStats();
+        saveTasks();
+    }
+};
+
+const toggleTaskComplete = (index) => {
+    tasks[index].completed = !tasks[index].completed;
+    console.log(tasks);
+    updateTasksList();
+    updateStats();
+    saveTasks();
+};
+
+const editTask = (index) => {
+    const taskInput = document.getElementById("taskInput");
+    taskInput.value = tasks[index].text;
+    tasks.splice(index, 1);
+    updateStats();
+    saveTasks();
+}
+const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    updateTasksList();
+    updateStats();
+    saveTasks();
+};
+
+const updateStats = () => {
+    const completeTasks = tasks.filter(task => task.completed).length;
+    const totalTasks = tasks.length;
+    const progress = (completeTasks / totalTasks) * 100;
+    const progressBar = document.getElementById('progress');
+    progressBar.style.width = `${progress}%`;
+    document.getElementById('numbers').innerText = `${completeTasks} / ${totalTasks}`;
+};
+
+
+const updateTasksList = () => {
+    let taskList = document.getElementById('task-list');
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const listItem = document.createElement('li');
+
+        listItem.innerHTML = `
+        <div class="taskItem">
+            <div class="task ${task.completed ? 'completed' : ''}">
+                <input type="checkbox" class="checkbox" ${task.completed ? "checked" : ""} onChange="toggleTaskComplete(${index})"/>
+                <p>${task.text}</p>
+            </div>
+            <div class="icons">
+                <img src="./img/edit.png" onClick="editTask(${index})"/>
+                <img src="./img/bin.png" onClick="deleteTask(${index})"/>
+            </div>
+        </div>
+        `; 
+        taskList.append(listItem);
+    });
+};
+
+document.getElementById("new-task").addEventListener("click", function(e){
+    e.preventDefault();
+    addTask();
+});
