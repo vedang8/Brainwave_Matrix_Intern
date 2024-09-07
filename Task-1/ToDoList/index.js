9f66acba76837cc6b719844564e41e001eef4b0d
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-09-07 11:45:01 +0530

    changes are done

--- Task-1/ToDoList/index.js
+++ Task-1/ToDoList/index.js
@@ -56,6 +56,10 @@ const updateStats = () => {
     const progressBar = document.getElementById('progress');
     progressBar.style.width = `${progress}%`;
     document.getElementById('numbers').innerText = `${completeTasks} / ${totalTasks}`;
+
+    if(tasks.length && completeTasks === totalTasks){
+        blaskConfetti();
+    }
 };
 
 
@@ -86,3 +90,45 @@ document.getElementById("new-task").addEventListener("click", function(e){
     e.preventDefault();
     addTask();
 });
+
+const blaskConfetti = () => {
+    const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+}
